6cb58a2f770ba522563d2fde81b25b02c168f707
Emiliano G Solazzi Griminger <133587754+Enertopia@users.noreply.github.com>
2023-12-16 17:39:49 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,12 @@
 # test_main.py
-# Placeholder for unit tests
+import unittest
+from app.main import calculate_taxes
+
+class TestMainFunctions(unittest.TestCase):
+    def test_calculate_taxes(self):
+        total_amount = 100
+        expected_taxes = 5  # Assuming a tax rate of 5%
+        self.assertEqual(calculate_taxes(total_amount), expected_taxes)
+
+if __name__ == '__main__':
+    unittest.main()
